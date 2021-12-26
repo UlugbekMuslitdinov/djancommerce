320143f13bcc2d50e93d46f5315fca148bc76bdd
Ulugbek <ulugbekmuslitdinov@gmail.com>
2021-12-27 00:16:07 +0500

    coverage configured

--- djancommerce/settings.py
+++ djancommerce/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd party apps
+    'django_nose',
+
     # Local
     'users.apps.UsersConfig',
 ]
@@ -121,3 +124,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
